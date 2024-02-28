0dfa34f9ad4d476dfc0028658b6ada9bc9d22fd1
Kelvin Orhungul <kelvin.orhungul@gmail.com>
2024-02-28 23:04:15 +0100

    task: reverse words

---
+++ src/reverseWords/index.js
@@ -0,0 +1,19 @@
+// Write a function in JavaScript that takes a string as input and returns a new string where each word in the input string is reversed.
+
+function reverseWords(inputStr) {
+    let result = "";
+    let wordsArr = inputStr.split(" ");
+
+    for (let i = 0; i < wordsArr.length; i++) {
+        let reversedWord = wordsArr[i].split("").reverse().join("");
+        console.log("reversedWord", reversedWord);
+        result = result.concat(reversedWord)
+        if (i < wordsArr.length - 1) {
+            result = result.concat(" ");
+        }
+        console.log("result", result)
+    }
+    return result;
+}
+
+export { reverseWords }
